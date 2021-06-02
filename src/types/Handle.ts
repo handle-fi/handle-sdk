5a8ac89ea44aa67bf3418c758a0dd6223d1f82c8
Lorenzo Fontoura <lorenzo@nioel.com>
2021-06-02 14:18:50 +1000

    add Handle type

---
+++ src/types/Handle.ts
@@ -0,0 +1,20 @@
+﻿import { fxToken } from "./fxToken";
+import { ethers } from "ethers";
+import { CollateralToken } from "./CollateralToken";
+
+/** Holds protocol data */
+export type Handle = {
+  fxTokens: fxToken[],
+  collateralTokens: CollateralToken[],
+  feeRecipient: string,
+  fees: {
+    /** Mint fee ratio per 1,000 */
+    mint: ethers.BigNumber,
+    /** Burn fee ratio per 1,000 */
+    burn: ethers.BigNumber,
+    /** Withdraw fee ratio per 1,000 */
+    withdraw: ethers.BigNumber,
+    /** Deposit fee ratio per 1,000 */
+    deposit: ethers.BigNumber
+  }
+};
