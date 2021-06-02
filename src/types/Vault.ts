3b7b3eb92bd78c44a0ec0f509e6a1dcecfac54f7
Lorenzo Fontoura <lorenzo@nioel.com>
2021-06-02 13:56:34 +1000

    add Vault type

---
+++ src/types/Vault.ts
@@ -0,0 +1,17 @@
+﻿import { fxToken } from "./fxToken";
+import { ethers } from "ethers";
+import { VaultCollateral } from "./VaultCollateral";
+
+export type Vault = {
+  token: fxToken,
+  debt: ethers.BigNumber,
+  collateral: VaultCollateral[],
+  collateralAsEth: ethers.BigNumber,
+  freeCollateralAsEth: ethers.BigNumber,
+  ratios: {
+    current: ethers.BigNumber,
+    minting: ethers.BigNumber,
+    /** Always 80% of the minting ratio, or the minimum possible value of 110% */
+    liquidation: ethers.BigNumber
+  }
+};
