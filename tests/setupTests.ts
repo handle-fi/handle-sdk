b29759d01c290eec0278c7734a750920a0d06377
Lorenzo Fontoura <lorenzo@nioel.com>
2021-06-03 14:16:58 +1000

    add test global setup with provider

---
+++ tests/setupTests.ts
@@ -0,0 +1,23 @@
+﻿import { ethers } from "ethers";
+import dotenv from "dotenv";
+
+let signer: ethers.Signer;
+
+export const getSigner = () => signer;
+
+global.beforeAll(() => {
+  dotenv.config();
+  // @ts-config
+  signer = new ethers.Wallet(
+    // @ts-ignore
+    process.env.PRIVATE_KEY,
+    new ethers.providers.InfuraWebSocketProvider(process.env.NETWORK, process.env.INFURA_KEY)
+  );
+});
+
+global.afterAll(() => {
+  // @ts-ignore
+  signer.provider._websocket.terminate();
+  // @ts-ignore
+  signer = null;
+});
