5106f034eea16294c83182835cf28cfe2c1a7e5e
Lorenzo Fontoura <lorenzo@nioel.com>
2021-06-04 09:59:53 +1000

    export all types in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,20 @@
-﻿import { SDK } from "./types/SDK";
-export default SDK;
+﻿import { SDK as HandleSDK } from "./types/SDK";
+import { fxTokens, CollateralTokens } from "./types/ProtocolTokens";
+import { fxToken } from "./types/fxToken";
+import { Token } from "./types/Token";
+import { Protocol } from "./types/Protocol";
+import { CollateralToken } from "./types/CollateralToken";
+import { Vault } from "./types/Vault";
+import { VaultCollateral } from "./types/VaultCollateral";
+
+export {
+  HandleSDK,
+  Protocol,
+  fxToken,
+  CollateralToken,
+  fxTokens,
+  CollateralTokens,
+  Vault,
+  VaultCollateral,
+  Token
+};
