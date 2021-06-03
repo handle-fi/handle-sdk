c4b577bc899b15a1be7e1f102f259ba0cac2d160
Lorenzo Fontoura <lorenzo@nioel.com>
2021-06-03 13:26:10 +1000

    update Vault.ts

--- src/types/Vault.ts
+++ src/types/Vault.ts
@@ -1,19 +1,42 @@
 ﻿import { fxToken } from "./fxToken";
 import { ethers } from "ethers";
 import { VaultCollateral } from "./VaultCollateral";
+import { SDK } from "./SDK";
 
-export type Vault = {
+export class Vault {
   /** Address for the owner of the vault */
-  account: string,
-  token: fxToken,
-  debt: ethers.BigNumber,
-  collateral: VaultCollateral[],
-  collateralAsEth: ethers.BigNumber,
-  freeCollateralAsEth: ethers.BigNumber,
-  ratios: {
-    current: ethers.BigNumber,
-    minting: ethers.BigNumber,
+  public account: string;
+  public token: fxToken;
+  public debt: ethers.BigNumber;
+  public collateral: VaultCollateral[];
+  public collateralAsEth: ethers.BigNumber;
+  public freeCollateralAsEth: ethers.BigNumber;
+  public ratios: {
+    current: ethers.BigNumber;
+    minting: ethers.BigNumber;
     /** Always 80% of the minting ratio, or the minimum possible value of 110% */
-    liquidation: ethers.BigNumber
+    liquidation: ethers.BigNumber;
+  };
+
+  private constructor(account: string, token: string, sdk: SDK) {
+    const fxToken = sdk.protocol.fxTokens.find((x) => x.address === token);
+    if (!fxToken) throw new Error(`Invalid fxToken address provided "${token}"`);
+    this.token = fxToken;
+    this.account = account;
+    this.debt = ethers.BigNumber.from(0);
+    this.collateral = [];
+    this.collateralAsEth = ethers.BigNumber.from(0);
+    this.freeCollateralAsEth = ethers.BigNumber.from(0);
+    this.ratios = {
+      current: ethers.BigNumber.from(0),
+      minting: ethers.BigNumber.from(0),
+      liquidation: ethers.BigNumber.from(0)
+    };
   }
-};
+
+  public static async from(account: string, token: string, sdk: SDK): Promise<Vault> {
+    const vault = new Vault(account, token, sdk);
+    // TODO: load vault data via SDK
+    return vault;
+  }
+}
