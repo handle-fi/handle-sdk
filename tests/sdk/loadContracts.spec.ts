b84f1547c57da0d5c27fc20a3cf8615fff43ef9e
Lorenzo Fontoura <lorenzo@nioel.com>
2021-06-03 15:14:10 +1000

    update loadContracts.spec.ts

---
+++ tests/sdk/loadContracts.spec.ts
@@ -0,0 +1,65 @@
+﻿import { describe, it } from "@jest/globals";
+import { SDK } from "../../src/types/SDK";
+import { getSigner } from "../setupTests";
+import { ethers } from "ethers";
+import { CollateralTokens, fxTokens } from "../../src/types/ProtocolTokens";
+
+let sdk: SDK;
+
+describe("SDK: loadContracts", () => {
+  it("Should create new SDK", async () => {
+    // @ts-ignore
+    sdk = await SDK.from(getSigner());
+  });
+  it("Should have loaded SDK provider and signer", async () => {
+    const networkExpected = process.env.NETWORK;
+    const network = await sdk.signer?.provider?.getNetwork();
+    if (!network) throw "invalid network";
+    expect(network.name).toEqual(networkExpected);
+  });
+  it("Should have loaded Handle contract", async () => {
+    const address = await sdk.contracts.handle.address;
+    expect(ethers.utils.isAddress(address)).toBeTruthy();
+  });
+  it("Should have loaded Comptroller contract", async () => {
+    const address = await sdk.contracts.comptroller.address;
+    expect(ethers.utils.isAddress(address)).toBeTruthy();
+  });
+  it("Should have loaded Treasury contract", async () => {
+    const address = await sdk.contracts.treasury.address;
+    expect(ethers.utils.isAddress(address)).toBeTruthy();
+  });
+  it("Should have loaded VaultLibrary contract", async () => {
+    const address = await sdk.contracts.vaultLibrary.address;
+    expect(ethers.utils.isAddress(address)).toBeTruthy();
+  });
+  it("Should have loaded fxKeeperPool contract", async () => {
+    const address = await sdk.contracts.fxKeeperPool.address;
+    expect(ethers.utils.isAddress(address)).toBeTruthy();
+  });
+  it("Should have loaded fxAUD contract", async () => {
+    const address = await sdk.contracts[fxTokens.fxAUD].address;
+    expect(ethers.utils.isAddress(address)).toBeTruthy();
+  });
+  it("Should have loaded fxEUR contract", async () => {
+    const address = await sdk.contracts[fxTokens.fxEUR].address;
+    expect(ethers.utils.isAddress(address)).toBeTruthy();
+  });
+  it("Should have loaded fxKRW contract", async () => {
+    const address = await sdk.contracts[fxTokens.fxKRW].address;
+    expect(ethers.utils.isAddress(address)).toBeTruthy();
+  });
+  it("Should have loaded wETH contract", async () => {
+    const address = await sdk.contracts[CollateralTokens.WETH].address;
+    expect(ethers.utils.isAddress(address)).toBeTruthy();
+  });
+  it("Should have loaded wBTC contract", async () => {
+    const address = await sdk.contracts[CollateralTokens.WBTC].address;
+    expect(ethers.utils.isAddress(address)).toBeTruthy();
+  });
+  it("Should have loaded DAI contract", async () => {
+    const address = await sdk.contracts[CollateralTokens.DAI].address;
+    console.log("address dai: ", address);
+    expect(ethers.utils.isAddress(address)).toBeTruthy();
+  });
+});
