494631f760f14ae141fb46ac8d8d527c93fefbae
Lorenzo Fontoura <lorenzo@nioel.com>
2021-06-03 16:48:59 +1000

    add fxtoken reader

---
+++ src/readers/fxTokens/index.ts
@@ -0,0 +1,25 @@
+﻿import query from "./query";
+import { mainneGqltClient, kovanGqlClient } from "../graphqlClient";
+import { ethers } from "ethers";
+
+export type IndexedFxlTokenData = {
+  name: string;
+  symbol: string;
+  rewardRatio: ethers.BigNumber;
+  totalSupply: ethers.BigNumber;
+  isValid: boolean;
+};
+
+/** Returns indexed vault data. */
+export const readFxTokens = async (isKovan: boolean): Promise<IndexedFxlTokenData[]> => {
+  const client = isKovan ? kovanGqlClient : mainneGqltClient;
+  const data = await client.request(query);
+  const tokens = data?.fxTokens;
+  if (tokens == null) throw new Error("Could not read fxTokens");
+  // Parse numbers.
+  for (let token of tokens) {
+    token.rewardRatio = ethers.BigNumber.from(token.rewardRatio);
+    token.totalSupply = ethers.BigNumber.from(token.totalSupply);
+  }
+  return tokens;
+};
---
+++ src/readers/fxTokens/query.ts
@@ -0,0 +1,13 @@
+﻿import { gql } from "graphql-request/dist";
+
+export default gql`
+  query {
+    fxTokens {
+      name
+      symbol
+      rewardRatio
+      totalSupply
+      isValid
+    }
+  }
+`;
