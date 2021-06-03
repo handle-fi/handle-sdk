bc3cfc30db58cdfdc54fc1ac9b81fe940e8e4c81
Lorenzo Fontoura <lorenzo@nioel.com>
2021-06-03 14:17:07 +1000

    add from.spec.ts

---
+++ tests/sdk/from.spec.ts
@@ -0,0 +1,13 @@
+﻿import { describe, it } from "@jest/globals";
+import { SDK } from "../../src/types/SDK";
+import { getSigner } from "../setupTests";
+
+let sdk: SDK;
+
+describe("SDK: from", () => {
+  it("Should create new SDK", async () => {
+    // @ts-ignore
+    sdk = await SDK.from(getSigner());
+    expect(sdk.protocol.fxTokens.length).toEqual(0);
+  });
+});
