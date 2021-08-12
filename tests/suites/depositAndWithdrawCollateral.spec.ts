5e5f610d452deb12ecb458590f4132e30ef36195
Lorenzo Fontoura <lorenzo.fontoura@dltx.io>
2021-08-12 12:27:35 +1000

    update deposit/withdraw unit test

--- tests/suites/depositAndWithdrawCollateral.spec.ts
+++ tests/suites/depositAndWithdrawCollateral.spec.ts
@@ -1,4 +1,4 @@
-﻿import { xdescribe, it } from "@jest/globals";
+﻿import { describe, it } from "@jest/globals";
 import { SDK } from "../../src/types/SDK";
 import { Vault } from "../../src/types/Vault";
 import { fxTokens, CollateralTokens } from "../../src/types/ProtocolTokens";
@@ -8,16 +8,21 @@ import { getSDK } from "../setupTests";
 let sdk: SDK;
 let vault: Vault;
 
-const DEPOSIT_AMOUNT = ethers.utils.parseEther("0.0000000000000001");
+const DEPOSIT_AMOUNT = ethers.BigNumber.from("1");
 const GAS_LIMIT = ethers.BigNumber.from("2500000");
 
-xdescribe("Vault: depositCollateral, withdrawCollateral", function () {
+describe("Vault: depositCollateral, withdrawCollateral", function () {
   beforeAll(async () => {
     sdk = getSDK();
     const account = await sdk.signer?.getAddress()!;
     vault = await Vault.from(account, fxTokens.fxAUD, sdk);
   });
   it("Should deposit eth", async () => {
+    await (
+      await vault.depositCollateral(DEPOSIT_AMOUNT, "ETH", false, GAS_LIMIT)
+    ).wait(1);
+  });
+  it("Should deposit weth", async () => {
     await (
       await vault.depositCollateral(DEPOSIT_AMOUNT, CollateralTokens.WETH, false, GAS_LIMIT)
     ).wait(1);
@@ -28,6 +33,11 @@ xdescribe("Vault: depositCollateral, withdrawCollateral", function () {
     ).wait(1);
   });
   it("Should withdraw eth", async () => {
+    await (
+      await vault.withdrawCollateral(DEPOSIT_AMOUNT, "ETH", false, GAS_LIMIT)
+    ).wait(1);
+  });
+  it("Should withdraw weth", async () => {
     await (
       await vault.withdrawCollateral(DEPOSIT_AMOUNT, CollateralTokens.WETH, false, GAS_LIMIT)
     ).wait(1);
