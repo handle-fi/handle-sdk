944c505c165b5eabbf8914c69185c5996e9399c9
Anthony Hinchliff <anthony.hinchliff@gmail.com>
2022-03-09 14:31:36 +1000

    v0.0.38 - update fx token urls

--- src/index.ts
+++ src/index.ts
@@ -41,13 +41,7 @@ import RewardPoolSDK from "./components/RewardPool";
 import { getIsKashiApproved, signKashiApproval } from "./utils/allowance-utils";
 import { getNetworkName } from "./utils/web3-utils";
 import { NETWORK_NAMES, SINGLE_COLLATERAL_NETWORK_NAMES } from "./constants";
-import ethereumTokenList from "./data/tokens/ethereum-tokens.json";
-import arbitrumTokenList from "./data/tokens/arbitrum-tokens.json";
-import polygonTokenList from "./data/tokens/polygon-tokens.json";
-
-const ETHEREUM_TOKEN_LIST = ethereumTokenList as TokenExtended<string>[];
-const ARBITRUM_TOKEN_LIST = arbitrumTokenList as TokenExtended<string>[];
-const POLYGON_TOKEN_LIST = polygonTokenList as TokenExtended<string>[];
+import { ETHEREUM_TOKEN_LIST, ARBITRUM_TOKEN_LIST, POLYGON_TOKEN_LIST } from "./data/tokens";
 
 export {
   FxTokensSDK,
