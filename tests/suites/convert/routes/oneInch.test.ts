5191c6ab5d36683a4d97dfea6374250041496ea4
Preston Harrison <preston@prestonharrison.com>
2022-05-13 13:42:43 +1000

    refactor tests

---
+++ tests/suites/convert/routes/oneInch.test.ts
@@ -0,0 +1,49 @@
+import { expect } from "chai";
+import { ethers } from "hardhat";
+import Convert from "../../../../src/components/Convert";
+import { getTokenDetails } from "../../../../src/utils/token-utils";
+import { sampleHlpTokenMethods } from "../sampleHlpTokenMethods";
+import { eth } from "../test-tokens";
+
+describe("oneInch route", () => {
+  describe("quote", () => {
+    it(`should return an api quote for arbitrum`, async () => {
+      const usdc = getTokenDetails("USDC", "arbitrum");
+      const usdt = getTokenDetails("USDT", "arbitrum");
+      const quote = await Convert.getQuote({
+        fromToken: { ...usdc, name: "" },
+        toToken: { ...usdt, name: "" },
+        network: "arbitrum",
+        connectedAccount: ethers.constants.AddressZero,
+        fromAmount: ethers.utils.parseEther("1"),
+        gasPrice: ethers.constants.One,
+        hlpMethods: sampleHlpTokenMethods
+      });
+      expect(quote).to.have.property("buyAmount");
+      expect(quote).to.have.property("sellAmount");
+    });
+  });
+  describe("swap", () => {
+    it(`should return an api swap`, async () => {
+      // void signer is used as forked chain is not used by api
+      const signer = new ethers.VoidSigner(
+        "0x82af49447d8a07e3bd95bd0d56f35241523fbab1",
+        ethers.provider
+      );
+      const usdt = getTokenDetails("USDT", "arbitrum");
+      const tx = await Convert.getSwap({
+        fromToken: { ...getTokenDetails("ETH", "arbitrum"), name: "Ethereum" },
+        toToken: { ...usdt, name: "Tether USD" },
+        network: "arbitrum",
+        connectedAccount: "0x82af49447d8a07e3bd95bd0d56f35241523fbab1",
+        gasPrice: ethers.utils.parseUnits("100", "gwei"), // very high gas price
+        hlpMethods: sampleHlpTokenMethods,
+        sellAmount: ethers.utils.parseUnits("1", eth.decimals),
+        buyAmount: ethers.utils.parseUnits("1", usdt.decimals),
+        signer,
+        slippage: 0.05
+      });
+      expect(tx).to.be.an("object");
+    });
+  });
+});
---
+++ tests/suites/convert/routes/zeroX.test.ts
@@ -0,0 +1,28 @@
+import { expect } from "chai";
+import { ethers } from "ethers";
+import { Network } from "../../../../src";
+import Convert from "../../../../src/components/Convert";
+import { getTokenDetails } from "../../../../src/utils/token-utils";
+import { sampleHlpTokenMethods } from "../sampleHlpTokenMethods";
+
+describe("zeroX route", () => {
+  describe("quote", () => {
+    (["ethereum", "polygon"] as Network[]).forEach((network) => {
+      it(`should return an api quote for ${network}`, async () => {
+        const usdc = getTokenDetails("USDC", network);
+        const usdt = getTokenDetails("USDT", network);
+        const quote = await Convert.getQuote({
+          fromToken: { ...usdc, name: "" },
+          toToken: { ...usdt, name: "" },
+          network,
+          connectedAccount: ethers.constants.AddressZero,
+          fromAmount: ethers.utils.parseEther("1"),
+          gasPrice: ethers.constants.One,
+          hlpMethods: sampleHlpTokenMethods
+        });
+        expect(quote).to.have.property("buyAmount");
+        expect(quote).to.have.property("sellAmount");
+      });
+    });
+  });
+});
