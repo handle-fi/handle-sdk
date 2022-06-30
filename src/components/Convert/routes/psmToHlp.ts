96bdf281a9b2e6262b8edc83565567947dc3914f
Preston Harrison <preston@prestonharrison.com>
2022-06-30 16:46:12 +1000

    psm to hlp quote

--- src/components/Convert/routes/psmToHlp.ts
+++ src/components/Convert/routes/psmToHlp.ts
@@ -1,7 +1,10 @@
-import { ethers } from "ethers";
+import { BigNumber, ethers } from "ethers";
 import { getTokenPegs } from "../../../utils/convert-utils";
 import { ConvertQuoteRouteArgs, ConvertTransactionRouteArgs, Quote } from "../Convert";
 import { PSM_TO_HLP, WeightInput } from "./weights";
+import psm from "./psm";
+import hlpSwap from "./hlpSwap";
+import HandleTokenManager from "../../TokenManager/HandleTokenManager";
 
 const psmToHlpWeight = async (input: WeightInput): Promise<number> => {
   const pegs = await getTokenPegs();
@@ -12,11 +15,32 @@ const psmToHlpWeight = async (input: WeightInput): Promise<number> => {
   return PSM_TO_HLP;
 };
 
-const psmToHlpQuoteHandler = async (input: ConvertQuoteRouteArgs): Promise<Quote> => {};
+const psmToHlpQuoteHandler = async (input: ConvertQuoteRouteArgs): Promise<Quote> => {
+  const pegs = await getTokenPegs();
+  const validPeg = pegs.find(
+    (peg) => peg.peggedToken.toLowerCase() === input.fromToken.address.toLowerCase()
+  );
+  if (!validPeg) throw new Error("No Valid Peg");
+  const fxToken = new HandleTokenManager().getTokenByAddress(validPeg.fxToken, input.network);
+  if (!fxToken) throw new Error("No fxToken found");
+
+  const psmQuote = await psm.quote({
+    ...input,
+    toToken: fxToken
+  });
+
+  return hlpSwap.quote({
+    ...input,
+    sellAmount: BigNumber.from(psmQuote.buyAmount),
+    fromToken: fxToken
+  });
+};
 
 const psmToHlpTransactionHandler = (
   input: ConvertTransactionRouteArgs
-): Promise<ethers.PopulatedTransaction> => {};
+): Promise<ethers.PopulatedTransaction> => {
+  return {} as any;
+};
 
 export default {
   weight: psmToHlpWeight,
